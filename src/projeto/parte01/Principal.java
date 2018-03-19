850474564f6bcb16c5bcb2140146968f814fe9a9
fabio.nunes <fabio.nunes@PC-DF-28.phd.local>
2018-03-19 10:57:37 -0300

    Configurando GIT

---
+++ src/projeto/parte01/Principal.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto.parte01;
+
+/**
+ *
+ * @author fabio.nunes
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Menu interativo
+        System.out.println("==== BEM VINDO AO NOSSO MENU INTERATIVO ====");
+        System.out.println("Escolha um número de 1 a 6 para entrar na opção desejada, e 7 para sair do programa.");
+        System.out.println("1. SOMA \n"
+                         + "2. SUBTRAÇÃO \n"
+                         + "3. DIVISÃO \n"
+                         + "4. FAIXA ETÁRIA \n"
+                         + "5. NÚMEROS PRIMOS \n"
+                         + "6. MUDANÇA DE BASE \n"
+                         + "7. SAIR");
+        //Declarando as variáveis
+        int op =0;
+        while(op > 7) {
+            
+        }
+    }
+    
+    
+    
+}
